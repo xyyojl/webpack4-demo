ff9d7b7e7765c81f3a5165e3c1417018d0e7726d
xyyojl <m13631897256@163.com>
2020-02-28 10:31:35 +0800

    使用 url-loader 打包图片，理解其最佳使用

--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,15 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.jpg$/,
+            test: /\.(jpg|png|gif)$/,
             use: {
-                loader: 'file-loader'
+                loader: 'url-loader',
+                options: {
+                    // placeholder 占位符
+                    name: '[name]_[hash].[ext]',
+                    outputPath: 'image/',
+                    limit: 204800
+                }
             }
         }]
     },
