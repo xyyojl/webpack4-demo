d04669da386a30522b4e8d0fce575081c91e803e
xyyojl <m13631897256@163.com>
2020-03-13 23:40:59 +0800

    使用 WebpackDevServer 实现请求转发

---
+++ devServer/webpack.config.js
@@ -0,0 +1,81 @@
+var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: './src/index.js'
+    },
+    devServer: {
+        contentBase: './dist',
+        open: true,
+        port: 8080,
+        hot: true,
+        hotOnly: true,
+        proxy: {
+            '/react/api': {
+                target: 'http://www.dell-lee.com/',
+                secure: false,
+                pathRewrite: {
+                    'header.json': 'demo.json'
+                },
+                changeOrigin: true,
+                headers: {
+                    host: 'www.dell-lee.com'
+                }
+            }
+        }
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        },{
+            test: /\.(jpg|png|gif)$/,
+            use: {
+                loader: 'url-loader',
+                options: {
+                    // placeholder 占位符
+                    name: '[name]_[hash].[ext]',
+                    outputPath: 'image/',
+                    limit: 204800
+                }
+            }
+        },{
+            test: /\.scss$/,
+            use: [
+                'style-loader',
+                {
+                    loader: 'css-loader',
+                    options: {
+                        importLoaders: 2,
+                        modules: true
+                    }
+                },
+                'sass-loader',
+                'postcss-loader'
+            ]
+        },{
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'postcss-loader'
+            ]
+        }]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: 'src/index.html'
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    ],
+    output: {
+        publicPath: '/',
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+}
