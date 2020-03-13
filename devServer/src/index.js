ed06c0dfaeb340f51a3f1d2665e885db275f7094
xyyojl <m13631897256@163.com>
2020-03-13 23:59:03 +0800

    使用 WebpackDevServer 解决单页面应用路由问题

--- devServer/src/index.js
+++ devServer/src/index.js
@@ -1,16 +1,21 @@
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 import axios from 'axios';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './home.js';
+import List from './list.js';
 
 class App extends Component{
-    componentDidMount(){
-        axios.get('/react/api/header.json')
-            .then((res) => {
-                console.log(res);
-            })
-    }
     render(){
-        return <div>Hello World</div>
+        return (
+            <BrowserRouter>
+                <div>
+                    <Route path="/"  exact component={Home}></Route>
+                    <Route path="/list" component={List}></Route>
+                </div>
+            </BrowserRouter>
+            
+        )
     }
 }
 ReactDom.render(<App />,document.getElementById('root'));
